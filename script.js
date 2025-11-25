582cef75813f9d83c5197037d83478a3d14bc3f8
KKumuu <mango13109@gmail.com>
2025-11-25 12:25:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿function triggerUpload(id) { document.getElementById(id).click(); }
+function loadImage(event, id) {
+    const imageBox = document.getElementById(id).parentElement;
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            imageBox.style.backgroundImage = `url(${e.target.result})`;
+            const span = imageBox.querySelector('span');
+            if(span) span.style.display = 'none';
+        }
+        reader.readAsDataURL(file);
+    }
+}
+function saveImage() {
+    const sheet = document.getElementById("sheet-container");
+    html2canvas(sheet).then(canvas => {
+        const link = document.createElement("a");
+        link.download = 'my_character_sheet.png';
+        link.href = canvas.toDataURL();
+        link.click();
+    });
+}
